b605b972385be5e9c0ff57e75a9a9c29850fbebb
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-01-29 09:14:20 +0100

    Lagt til vedtak og logikk for å hente gjeldende vedtak

---
+++ app/src/main/kotlin/no/nav/omsorgsdager/vedtak/Vedtak.kt
@@ -0,0 +1,41 @@
+package no.nav.omsorgsdager.vedtak
+
+import no.nav.omsorgsdager.tid.Periode
+import no.nav.omsorgsdager.tid.Tidslinje
+import java.time.ZonedDateTime
+
+internal interface Vedtak {
+    val status: VedtakStatus
+    val statusSistEndret: ZonedDateTime
+    val barn: Any
+    val periode: Periode
+    fun kopiMedNyPeriode(nyPeriode: Periode) : Vedtak
+
+    companion object {
+        internal fun List<Vedtak>.gjeldendeVedtak() = asSequence()
+            .filterNot { it.status == VedtakStatus.FORSLAG }
+            .sortedByDescending { it.statusSistEndret }
+            .groupBy { it.barn }
+            .map { it.value.gjeldendeVedtakPerBarn() }
+            .flatten()
+            .toList()
+
+        private fun List<Vedtak>.gjeldendeVedtakPerBarn() : List<Vedtak> {
+            if (isEmpty()) return emptyList()
+            require(all { it.barn == first().barn }) { "Kan kun gjøres for samme barn" }
+
+            val gjeldendeVedtak = mutableListOf<Vedtak>()
+
+            forEach { vedtak ->
+                Tidslinje(gjeldendeVedtak.map { it.periode })
+                    .leggTil(vedtak.periode)
+                    .nyePerioder()
+                    .forEach { nyPeriode ->
+                        gjeldendeVedtak.add(vedtak.kopiMedNyPeriode(nyPeriode = nyPeriode))
+                    }
+            }
+
+            return gjeldendeVedtak
+        }
+    }
+}
