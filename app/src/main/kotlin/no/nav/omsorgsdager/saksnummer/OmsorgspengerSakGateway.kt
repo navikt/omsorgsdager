268ffcdc18439a82e2f86ce25fdea155bd2acbea
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-03-01 13:38:20 +0100

    Flytter ut helsesjekklogikk med azure integrasjon

--- app/src/main/kotlin/no/nav/omsorgsdager/saksnummer/OmsorgspengerSakGateway.kt
+++ app/src/main/kotlin/no/nav/omsorgsdager/saksnummer/OmsorgspengerSakGateway.kt
@@ -1,16 +1,10 @@
 package no.nav.omsorgsdager.saksnummer
 
-import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
-import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
-import com.nimbusds.jwt.SignedJWT
 import io.ktor.http.*
-import no.nav.helse.dusseldorf.ktor.health.HealthCheck
-import no.nav.helse.dusseldorf.ktor.health.Healthy
-import no.nav.helse.dusseldorf.ktor.health.Result
-import no.nav.helse.dusseldorf.ktor.health.UnHealthy
 import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
 import no.nav.helse.dusseldorf.oauth2.client.CachedAccessTokenClient
+import no.nav.omsorgsdager.AzureAwareGateway
 import no.nav.omsorgsdager.CorrelationId
 import no.nav.omsorgsdager.Identitetsnummer
 import no.nav.omsorgsdager.OmsorgspengerSaksnummer
@@ -19,13 +13,15 @@ import org.json.JSONObject
 import java.net.URI
 
 internal class OmsorgspengerSakGateway(
-    private val accessTokenClient: AccessTokenClient,
-    private val hentSaksnummerFraOmsorgspengerSakScopes: Set<String>,
-    private val omsorgspengerSakUrl: URI
-) : HealthCheck {
-
-    private val cachedAccessTokenClient = CachedAccessTokenClient(accessTokenClient)
-    private val pingUrl = "$omsorgspengerSakUrl/isready"
+    accessTokenClient: AccessTokenClient,
+    hentSaksnummerFraOmsorgspengerSakScopes: Set<String>,
+    omsorgspengerSakUrl: URI
+) : AzureAwareGateway(
+    navn = "OmsorgspengerSakGateway",
+    accessTokenClient = accessTokenClient,
+    scopes = hentSaksnummerFraOmsorgspengerSakScopes,
+    pingUri = URI("$omsorgspengerSakUrl/isready")) {
+    
     private val omsorgspengerSaksnummerUrl = "$omsorgspengerSakUrl/saksnummer"
 
     internal suspend fun hentSaksnummer(
@@ -54,36 +50,4 @@ internal class OmsorgspengerSakGateway(
         )
 
     }
-
-    private fun authorizationHeader() =
-        cachedAccessTokenClient.getAccessToken(hentSaksnummerFraOmsorgspengerSakScopes).asAuthoriationHeader()
-
-    override suspend fun check() =
-        Result.merge(
-            "OmsorgspengerSakGateway",
-            accessTokenCheck(),
-            pingOmsorgspengerSakCheck()
-        )
-
-    private fun accessTokenCheck() = kotlin.runCatching {
-        accessTokenClient.getAccessToken(hentSaksnummerFraOmsorgspengerSakScopes).let {
-            (SignedJWT.parse(it.accessToken).jwtClaimsSet.getStringArrayClaim("roles")?.toList()
-                ?: emptyList()).contains("access_as_application")
-        }
-    }.fold(
-        onSuccess = {
-            when (it) {
-                true -> Healthy("AccessTokenCheck", "OK")
-                false -> UnHealthy("AccessTokenCheck", "Feil: Mangler rettigheter")
-            }
-        },
-        onFailure = { UnHealthy("AccessTokenCheck", "Feil: ${it.message}") }
-    )
-
-
-    private suspend fun pingOmsorgspengerSakCheck() =
-        pingUrl.httpGet().awaitStringResponseResult().third.fold(
-            success = { Healthy("PingOmsorgspengerSak", "OK: $it") },
-            failure = { UnHealthy("PingOmsorgspengerSak", "Feil: ${it.message}") }
-        )
 }
