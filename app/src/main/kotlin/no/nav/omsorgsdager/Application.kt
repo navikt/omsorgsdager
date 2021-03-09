e64714582d6d640ebc24c1aca8f8488a2acb9219
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-03-09 13:09:32 +0100

    Ble ikke startet på API_ONLY

--- app/src/main/kotlin/no/nav/omsorgsdager/Application.kt
+++ app/src/main/kotlin/no/nav/omsorgsdager/Application.kt
@@ -1,6 +1,5 @@
 package no.nav.omsorgsdager
 
-import io.ktor.application.*
 import no.nav.helse.rapids_rivers.KtorBuilder
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.k9.rapid.river.hentOptionalEnv
@@ -10,23 +9,17 @@ fun main() {
     val applicationContext = ApplicationContext.Builder().build()
 
     when (applicationContext.env.hentOptionalEnv("APP_MODE")) {
-
-        "API_ONLY" -> KtorBuilder()
-            .log(LoggerFactory.getLogger("no.nav.omsorgdager.api_only"))
-            .port(applicationContext.env.hentOptionalEnv("HTTP_PORT")?.toInt() ?: 8080)
-            .liveness { true }
-            .readiness { true }
-            .module { omsorgsdager(applicationContext) }
-            .module {
-                environment.monitor.subscribe(ApplicationStarting) {
-                    applicationContext.start()
-                }
-                environment.monitor.subscribe(ApplicationStopping) {
-                    applicationContext.stop()
-                }
-            }
-            .build()
-            .start(wait = false)
+        "API_ONLY" -> {
+            applicationContext.start()
+            KtorBuilder()
+                .log(LoggerFactory.getLogger("no.nav.omsorgdager.api_only"))
+                .port(applicationContext.env.hentOptionalEnv("HTTP_PORT")?.toInt() ?: 8080)
+                .liveness { true }
+                .readiness { true }
+                .module { omsorgsdager(applicationContext) }
+                .build()
+                .start(wait = false)
+        }
 
         else -> RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(applicationContext.env))
             .withKtorModule { omsorgsdager(applicationContext) }
