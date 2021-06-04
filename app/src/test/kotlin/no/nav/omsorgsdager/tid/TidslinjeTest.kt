e5cef1e7692ad7013b7c4435ab8162764e8714fa
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-06-04 10:31:30 +0200

    Legge til feilende tester for Tidslinje som ikke håndterer å slå sammen til lengst mulige perioder.

--- app/src/test/kotlin/no/nav/omsorgsdager/tid/TidslinjeTest.kt
+++ app/src/test/kotlin/no/nav/omsorgsdager/tid/TidslinjeTest.kt
@@ -1,5 +1,7 @@
 package no.nav.omsorgsdager.tid
 
+import no.nav.omsorgsdager.tid.Periode.Companion.dato
+import no.nav.omsorgsdager.tid.Periode.Companion.periode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -22,7 +24,7 @@ internal class TidslinjeTest {
         assertThat(tidslinje.nyePerioder()).hasSameElementsAs(setOf(
             År1999,
             Periode("2022-01-01/2022-01-31"),
-            Periode("2022-03-01/2022-12-31"),
+            Periode("2022-03-01/2022-12-31")
         ))
     }
 
@@ -65,7 +67,56 @@ internal class TidslinjeTest {
             .leggTil(År1999TilTideneseEnde)
 
         assertThat(tidslinje.nyePerioder()).hasSameElementsAs(setOf(
-            Periode("1999-06-02/2020-05-31"),
+            Periode("1999-06-02/2020-05-31")
+        ))
+    }
+
+    @Test
+    fun `Mange perioder til tidenes ende`() {
+        val periode0 = "2021-01-10/2021-03-15".periode()
+        val periode1 = "2022-04-12/9999-12-31".periode()
+        val periode2 = "2025-04-12/9999-12-31".periode()
+        val periode3 = "1999-07-02/9999-12-31".periode()
+
+        val tidslinje = Tidslinje(listOf(periode0))
+            .leggTil(periode1)
+            .leggTil(periode2)
+            .leggTil(periode3)
+
+        assertThat(tidslinje.nyePerioder()).hasSameElementsAs(setOf(
+            "1999-07-02/2021-01-09".periode(),
+            "2021-03-16/9999-12-31".periode()
+        ))
+    }
+
+    @Test
+    fun `Perioder som til slutt blir en lang periode`() {
+        val periode0 = Periode("2021-03-03".dato())
+        val periode1 = Periode("2021-03-01".dato())
+        val periode2 = Periode("2021-02-03/2021-02-27")
+        val periode3 = Periode("2021-03-02".dato())
+        val periode4 = Periode("2021-02-28".dato())
+
+        val tidslinje = Tidslinje(emptyList())
+        assertThat(tidslinje.nyePerioder()).isEmpty()
+        assertThat(tidslinje.leggTil(periode0).nyePerioder()).hasSameElementsAs(setOf(
+            "2021-03-03/2021-03-03".periode()
+        ))
+        assertThat(tidslinje.leggTil(periode1).nyePerioder()).hasSameElementsAs(setOf(
+            "2021-03-03/2021-03-03".periode(),
+            "2021-03-01/2021-03-01".periode()
+        ))
+        assertThat(tidslinje.leggTil(periode2).nyePerioder()).hasSameElementsAs(setOf(
+            "2021-03-03/2021-03-03".periode(),
+            "2021-03-01/2021-03-01".periode(),
+            "2021-02-03/2021-02-27".periode()
+        ))
+        assertThat(tidslinje.leggTil(periode3).nyePerioder()).hasSameElementsAs(setOf(
+            "2021-03-01/2021-03-03".periode(),
+            "2021-02-03/2021-02-27".periode()
+        ))
+        assertThat(tidslinje.leggTil(periode4).nyePerioder()).hasSameElementsAs(setOf(
+            "2021-02-03/2021-03-03".periode()
         ))
     }
 }
