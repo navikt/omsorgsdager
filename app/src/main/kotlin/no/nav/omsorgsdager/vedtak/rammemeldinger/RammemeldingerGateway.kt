ba940b8c41c12f8b820d62baeae7c5043e81bfc3
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2021-06-22 12:07:52 +0200

    Integrasjon for å hente rammemeldinger & nais-config

--- app/src/main/kotlin/no/nav/omsorgsdager/vedtak/rammemeldinger/RammemeldingerGateway.kt
+++ app/src/main/kotlin/no/nav/omsorgsdager/vedtak/rammemeldinger/RammemeldingerGateway.kt
@@ -1,10 +1,21 @@
 package no.nav.omsorgsdager.vedtak.rammemeldinger
 
+import io.ktor.client.request.*
+import io.ktor.http.*
+import no.nav.helse.dusseldorf.ktor.client.SimpleHttpClient.httpGet
+import no.nav.helse.dusseldorf.ktor.client.SimpleHttpClient.readTextOrThrow
 import no.nav.helse.dusseldorf.oauth2.client.AccessTokenClient
 import no.nav.omsorgsdager.AzureAwareGateway
 import no.nav.omsorgsdager.CorrelationId
+import no.nav.omsorgsdager.Identitetsnummer.Companion.somIdentitetsnummer
 import no.nav.omsorgsdager.OmsorgspengerSaksnummer
+import no.nav.omsorgsdager.tid.Periode
+import no.nav.omsorgsdager.tid.Periode.Companion.dato
+import no.nav.omsorgsdager.tid.Periode.Companion.tidspunkt
 import no.nav.omsorgsdager.vedtak.dto.AleneOmsorgInnvilgetVedtak
+import no.nav.omsorgsdager.vedtak.dto.Barn
+import no.nav.omsorgsdager.vedtak.dto.Kilde
+import org.json.JSONObject
 import java.net.URI
 
 internal class RammemeldingerGateway(
@@ -17,9 +28,39 @@ internal class RammemeldingerGateway(
     scopes = scopes,
     pingUri = URI("$baseUrl/isready")) {
 
+    private val hentAleneOmsorgUrl = "$baseUrl/alene-om-omsorgen?saksnummer="
+
     internal suspend fun hentAleneOmsorg(
         saksnummer: OmsorgspengerSaksnummer,
         correlationId: CorrelationId) : List<AleneOmsorgInnvilgetVedtak> {
-        return emptyList()
+
+        val urlMedSaksnummer = "${hentAleneOmsorgUrl}${saksnummer}"
+
+        val (httpStatusCode, response) = urlMedSaksnummer.httpGet { builder ->
+            builder.header(HttpHeaders.Authorization, authorizationHeader())
+            builder.header(HttpHeaders.XCorrelationId, "$correlationId")
+            builder.accept(ContentType.Application.Json)
+        }.readTextOrThrow()
+
+        require(httpStatusCode == HttpStatusCode.OK) {
+            "HTTP ${httpStatusCode.value} fra $urlMedSaksnummer. Response=$response"
+        }
+
+        return JSONObject(response).getJSONArray("aleneOmOmsorgen").map { it as JSONObject }.map { aleneOm -> AleneOmsorgInnvilgetVedtak(
+            tidspunkt = aleneOm.getString("registrert").tidspunkt(),
+            periode = Periode(
+                fom = aleneOm.getString("gyldigFraOgMed").dato(),
+                tom = aleneOm.getString("gyldigTilOgMed").dato()
+            ),
+            barn = aleneOm.getJSONObject("barn").let { barn -> Barn(
+                identitetsnummer = barn.getString("identitetsnummer").somIdentitetsnummer(),
+                fødselsdato = barn.getString("fødselsdato").dato()
+            )},
+            kilder = aleneOm.getJSONObject("kilde").let { kilde -> setOf(Kilde(
+                id = kilde.getString("id"),
+                type = kilde.getString("type")
+            ))}
+        )}
+
     }
 }
